0f1d3fef4ef59dc10039a4c2fbca3beffe04110b
Alexander Yermolenko <yaa.mbox@gmail.com>
2017-12-24 22:52:06 +0200

    Add keyboard setup

---
+++ bin/kb-reset.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#
+#  kb-reset.sh - reset keyboard map using setxkbmap
+#
+#  Copyright (C) 2013, 2017 Alexander Yermolenko <yaa.mbox@gmail.com>
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+die()
+{
+    local msg=${1:-"Unknown error"}
+    hash zenity 2>/dev/null && \
+        zenity --error --title "Error" --text "ERROR: $msg"
+    echo "ERROR: $msg" 1>&2
+    exit 1
+}
+
+require()
+{
+    local cmd=${1:?"Command name is required"}
+    local extra_info=${2:+"\nNote: $2"}
+    hash $cmd 2>/dev/null || die "$cmd not found$extra_info"
+}
+
+require setxkbmap
+
+killall xcape 2>/dev/null
+
+setxkbmap \
+    -model pc105 \
+    -layout us \
+    -option ''
+
