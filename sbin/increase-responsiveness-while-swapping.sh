9b04cfde4ffa22dbf2cb874710ef7c6673e22731
Alexander Yermolenko <yaa.mbox@gmail.com>
2018-05-13 18:38:41 +0300

    Add a script to increase responsiveness when swapping
    The script tunes VM subsystem to (hopefully) increase responsiveness when swapping

---
+++ sbin/increase-responsiveness-while-swapping.sh
@@ -0,0 +1,13 @@
+sysctl vm.min_free_kbytes
+#sysctl vm.min_free_kbytes=65536
+#sysctl vm.min_free_kbytes=131072
+sysctl vm.min_free_kbytes=256000
+
+sysctl vm.swappiness
+#sysctl vm.swappiness=30
+#sysctl vm.swappiness=5
+sysctl vm.swappiness=20
+
+sysctl vm.vfs_cache_pressure
+#sysctl vm.vfs_cache_pressure=50
+sysctl vm.vfs_cache_pressure=25
