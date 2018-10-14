f058879c0ed66bc122da75a57844d44daed575ec
Alexander Yermolenko <yaa.mbox@gmail.com>
2018-10-14 20:54:58 +0300

    Check permissions before running the admin script

--- sbin/increase-responsiveness-while-swapping.sh
+++ sbin/increase-responsiveness-while-swapping.sh
@@ -1,3 +1,14 @@
+#!/bin/bash
+
+die()
+{
+    msg=${1:-"Unknown Error"}
+    echo "ERROR: $msg" 1>&2
+    exit 1
+}
+
+[ "$EUID" -eq 0 ] || die "Superuser privileges required"
+
 sysctl vm.min_free_kbytes
 #sysctl vm.min_free_kbytes=65536
 #sysctl vm.min_free_kbytes=131072
