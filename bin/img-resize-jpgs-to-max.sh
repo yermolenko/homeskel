0ca254f342188e0217595f713f5fdb8baf1da9e6
Alexander Yermolenko <yaa.mbox@gmail.com>
2018-10-28 18:16:18 +0200

    Add scripts for batch shrinking of JPG images to fit into the size given

---
+++ bin/img-resize-jpgs-to-max.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+max_size=${1:-1000}
+
+echo -n "Checking the terminal ..." >&2 && [ -t 0 -a -t 2 ] && echo " OK" >&2 && \
+    read -p "Are you sure you want to resize JPG files in $PWD to max $max_size px? " -n 1 && \
+    [[ $REPLY =~ ^[Yy]$ ]] && echo >&2 || { echo " Exiting." >&2; exit 1; }
+
+echo -n "Resizing JPG files in $PWD to max $max_size px ... "
+
+find . -type f -and \( -name "*.[Jj][Pp][Gg]" \) \
+     -exec sh -c "mv \"{}\" \"{}.tmp.jpg\" && convert \"{}.tmp.jpg\" -units PixelsPerInch -density 100 -resize \"$max_size>\" \"{}\" && rm \"{}.tmp.jpg\"" \;
+
+echo "DONE."
