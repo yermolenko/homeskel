d99fbd8bec48fbc7785948cdab8452ea78f2feef
Alexander Yermolenko <yaa.mbox@gmail.com>
2020-08-30 17:07:41 +0300

    Add some org-mode customizations

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -167,10 +167,23 @@
 (global-set-key (kbd "C-x g") 'magit-status)
 (global-set-key (kbd "C-x M-g") 'magit-dispatch-popup)
 
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cc" 'org-capture)
+(global-set-key "\C-cb" 'org-switchb)
+
 (ffap-bindings)
 
 (when (fboundp 'windmove-default-keybindings)
   (windmove-default-keybindings))
+
+;; --------------------
+
+(defun org-summary-todo (n-done n-not-done)
+  "Switch entry to DONE when all subentries are done, to TODO otherwise."
+  (let (org-log-done org-log-states)   ; turn off logging
+    (org-todo (if (= n-not-done 0) "DONE" "TODO"))))
+(add-hook 'org-after-todo-statistics-hook 'org-summary-todo)
 
 ;; --------------------
 
