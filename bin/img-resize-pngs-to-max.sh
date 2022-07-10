10b9cf26bc7cad1ee47b099259a2cc5403279b05
Alexander Yermolenko <yaa.mbox@gmail.com>
2022-07-10 09:27:08 +0300

    Add a script for batch shrinking of PNG images to fit into the size given

---
+++ bin/img-resize-pngs-to-max.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+max_size=${1:-1000}
+
+echo -n "Checking the terminal ..." >&2 && [ -t 0 -a -t 2 ] && echo " OK" >&2 && \
+    read -p "Are you sure you want to resize PNG files in $PWD to max $max_size px? " -n 1 && \
+    [[ $REPLY =~ ^[Yy]$ ]] && echo >&2 || { echo " Exiting." >&2; exit 1; }
+
+echo -n "Resizing PNG files in $PWD to max $max_size px ... "
+
+find . -type f -and \( -name "*.[Pp][Nn][Gg]" \) \
+     -exec sh -c "mv \"{}\" \"{}.tmp.png\" && convert \"{}.tmp.png\" -units PixelsPerInch -density 100 -resize \"$max_size>\" \"{}\" && rm \"{}.tmp.png\"" \;
+
+echo "DONE."
