c9018fcdc35b5a3fabcef97b971c5fe1b2fc187e
Alexander Yermolenko <yaa.mbox@gmail.com>
2022-09-25 13:45:16 +0300

    Simplify keyboard setup scripts

--- bin/kb-us-altgr-intl.sh
+++ bin/kb-us-altgr-intl.sh
@@ -1,53 +1,7 @@
 #!/bin/bash
-#
-#  kb-us-altgr-intl.sh - setup keyboard for English (altgr-intl
-#  variant) using setxkbmap
-#
-#  Copyright (C) 2013, 2017, 2020, 2022 Alexander Yermolenko
-#  <yaa.mbox@gmail.com>
-#
-#  This program is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 3 of the License, or
-#  (at your option) any later version.
-#
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
 
-die()
-{
-    local msg=${1:-"Unknown error"}
-    hash zenity 2>/dev/null && \
-        zenity --error --title "Error" --text "ERROR: $msg"
-    echo "ERROR: $msg" 1>&2
-    exit 1
-}
+source kb-setup-common.sh || exit 1
 
-require()
-{
-    local cmd=${1:?"Command name is required"}
-    local extra_info=${2:+"\nNote: $2"}
-    hash $cmd 2>/dev/null || die "$cmd not found$extra_info"
-}
-
-require setxkbmap
-require kb-spc2ctrl.sh
-
-setxkbmap \
-    -model pc105 \
-    -layout us \
-    -option ''
-sleep 2
-setxkbmap \
-    -model pc105 \
-    -layout us \
-    -variant altgr-intl \
-    -option 'ctrl:nocaps'
-
-kb-spc2ctrl.sh
+kb_reset
+kb_us_altgr_intl
+space_as_control
