321e2f3493067afa78f8542dccc702a88bd01fc5
Alexander Yermolenko <yaa.mbox@gmail.com>
2022-09-25 11:53:48 +0300

    Simplify scripts for finding files matching various criteria

--- bin/find-recentmod-minutes.sh
+++ bin/find-recentmod-minutes.sh
@@ -3,16 +3,9 @@
 minutes=${1:-5}
 dirname=${2:-.}
 
+echo "Searching for modifications in $dirname during last $minutes minutes ..."
+
 find "$dirname" -mount \
      -path "./lost+found" -prune -o \
      -path "./.avfs" -prune -o \
-     -path "./.mozilla" -prune -o \
-     -path "./.icedove" -prune -o \
      -mmin -$minutes -print
-
-     # -path "./proc" -prune -o \
-     # -path "./sys" -prune -o \
-     # -path "./run" -prune -o \
-     # -path "./var/run" -prune -o \
-     # -path "./dev" -prune -o \
-     # -path "./mnt" -prune -o \
