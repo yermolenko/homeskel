6239adb0f7f05b7981b68097ef48d4b3790c7095
Alexander Yermolenko <yaa.mbox@gmail.com>
2024-11-17 17:19:04 +0200

    Make sharing the current directory via HTTP compatible with Python 3

--- bin/share-cwd-via-http-8008.sh
+++ bin/share-cwd-via-http-8008.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-python -m SimpleHTTPServer 8008
+hash python3 >/dev/null 2>&1 && \
+    python3 -m http.server 8008 || \
+        python -m SimpleHTTPServer 8008
--- bin/share-cwd-via-http.sh
+++ bin/share-cwd-via-http.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-python -m SimpleHTTPServer
+hash python3 >/dev/null 2>&1 && \
+    python3 -m http.server || \
+        python -m SimpleHTTPServer
