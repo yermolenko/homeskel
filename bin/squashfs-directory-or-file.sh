b303246a9ba680f8da1945fcdb7ca3460668edd7
Alexander Yermolenko <yaa.mbox@gmail.com>
2025-12-28 16:08:02 +0200

    Make possible to append to the existing squashfs

--- bin/squashfs-directory-or-file.sh
+++ bin/squashfs-directory-or-file.sh
@@ -5,7 +5,17 @@ archive_filename=${2:-./`basename "$item"`.squashfs}
 
 [ -e "$item" ] || { echo "ERROR: \"$item\" does not exist"; exit 1; }
 
-[ -e "$archive_filename" ] && { echo "ERROR: Destination already exists"; exit 1; }
+[ -e "$archive_filename" ] && \
+    {
+        echo "WARNING: Destination already exists";
+
+        echo -n "Checking the terminal ..." >&2 && [ -t 0 -a -t 2 ] && echo " OK" >&2 && \
+            read -p "Are you sure to APPEND to the existing squashfs? " -n 1 && \
+            [[ $REPLY =~ ^[Yy]$ ]] && echo >&2 || { echo " Exiting." >&2; exit 1; }
+
+        echo ""
+        echo  "Appending to the existing squashfs ... "
+    }
 
 mksquashfs "$item" "$archive_filename" -processors 2 -comp xz
 
